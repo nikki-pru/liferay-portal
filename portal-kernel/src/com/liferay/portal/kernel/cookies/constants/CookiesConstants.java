741ec5669f8938e9b27cf63497c86ed3e32d0db8
Manuele Castro <manuele.castro@liferay.com>
2024-01-11 22:44:06 +0000

    LPD-1909 Add new cookies constants

--- portal-kernel/src/com/liferay/portal/kernel/cookies/constants/CookiesConstants.java
+++ portal-kernel/src/com/liferay/portal/kernel/cookies/constants/CookiesConstants.java
@@ -53,6 +53,12 @@ public class CookiesConstants {
 
 	public static final String NAME_REMEMBER_ME = "REMEMBER_ME";
 
+	public static final String NAME_REMEMBER_ME_TOKEN_ID =
+		"REMEMBER_ME_TOKEN_ID";
+
+	public static final String NAME_REMEMBER_ME_TOKEN_TOKEN =
+		"REMEMBER_ME_TOKEN_TOKEN";
+
 	public static final String NAME_REMOTE_PREFERENCE_PREFIX =
 		"REMOTE_PREFERENCE_";
 
