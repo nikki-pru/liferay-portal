f2e44b032b6d34dec1a763571ffd981caa75460a
Selton Guedes <selton.guedes@liferay.com>
2024-02-22 14:44:52 -0300

    LPD-16334 Use KaleoDefinitionService instead of KaleoDefinitionLocalService
     # breaking
    
     ## What modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/WorkflowDefinitionManagerImpl.java
    
     modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/WorkflowDefinitionManagerImpl.java now invokes the Service instead of the Local Service
    
     ## Why
    
     at present, users without permission can access a workflow definition via the API
    
    ----

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/WorkflowDefinitionManagerImpl.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/WorkflowDefinitionManagerImpl.java
@@ -29,6 +29,7 @@ import com.liferay.portal.workflow.kaleo.runtime.WorkflowEngine;
 import com.liferay.portal.workflow.kaleo.runtime.integration.internal.util.WorkflowLockUtil;
 import com.liferay.portal.workflow.kaleo.runtime.util.comparator.KaleoDefinitionOrderByComparator;
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionLocalService;
+import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionService;
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionLocalService;
 import com.liferay.portal.workflow.manager.WorkflowDefinitionManager;
 
@@ -92,7 +93,7 @@ public class WorkflowDefinitionManagerImpl
 			serviceContext.setCompanyId(companyId);
 
 			List<KaleoDefinition> kaleoDefinitions =
-				_kaleoDefinitionLocalService.getScopeKaleoDefinitions(
+				_kaleoDefinitionService.getScopeKaleoDefinitions(
 					WorkflowDefinitionConstants.SCOPE_ALL, true, start, end,
 					KaleoDefinitionOrderByComparator.getOrderByComparator(
 						orderByComparator, _kaleoWorkflowModelConverter),
@@ -172,7 +173,7 @@ public class WorkflowDefinitionManagerImpl
 			serviceContext.setCompanyId(companyId);
 
 			return _kaleoWorkflowModelConverter.toWorkflowDefinition(
-				_kaleoDefinitionLocalService.getKaleoDefinition(
+				_kaleoDefinitionService.getKaleoDefinition(
 					name, serviceContext));
 		}
 		catch (WorkflowException workflowException) {
@@ -198,7 +199,7 @@ public class WorkflowDefinitionManagerImpl
 
 			if (active == null) {
 				kaleoDefinitions =
-					_kaleoDefinitionLocalService.getScopeKaleoDefinitions(
+					_kaleoDefinitionService.getScopeKaleoDefinitions(
 						WorkflowDefinitionConstants.SCOPE_ALL, start, end,
 						KaleoDefinitionOrderByComparator.getOrderByComparator(
 							orderByComparator, _kaleoWorkflowModelConverter),
@@ -206,7 +207,7 @@ public class WorkflowDefinitionManagerImpl
 			}
 			else {
 				kaleoDefinitions =
-					_kaleoDefinitionLocalService.getScopeKaleoDefinitions(
+					_kaleoDefinitionService.getScopeKaleoDefinitions(
 						WorkflowDefinitionConstants.SCOPE_ALL, active, start,
 						end,
 						KaleoDefinitionOrderByComparator.getOrderByComparator(
@@ -254,7 +255,7 @@ public class WorkflowDefinitionManagerImpl
 
 		try {
 			return _kaleoWorkflowModelConverter.toWorkflowDefinition(
-				_kaleoDefinitionLocalService.getKaleoDefinition(
+				_kaleoDefinitionService.getKaleoDefinition(
 					workflowDefinitionId));
 		}
 		catch (NoSuchModelException noSuchModelException) {
@@ -494,6 +495,9 @@ public class WorkflowDefinitionManagerImpl
 
 	@Reference
 	private KaleoDefinitionLocalService _kaleoDefinitionLocalService;
+
+	@Reference
+	private KaleoDefinitionService _kaleoDefinitionService;
 
 	@Reference
 	private KaleoDefinitionVersionLocalService
