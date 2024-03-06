5981f154290f0216ade7d3c6356a829c2ed2d7d3
Roberto Díaz <roberto.diaz@liferay.com>
2024-03-06 20:42:11 +0100

    LPD-19203 Revert "LPS-205268 Replace only within bounds"
    This reverts commit fca174b4d82cb44d1597df98def0479ca6e6e581.

--- modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/main/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformer.java
+++ modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/main/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformer.java
@@ -234,13 +234,13 @@ public class AMBackwardsCompatibilityHtmlContentTransformer
 		while (lastPos < end) {
 			int imgStart = html.indexOf(_OPEN_TAG_TOKEN_IMG, lastPos);
 
-			if ((imgStart == -1) || (imgStart > end)) {
+			if (imgStart == -1) {
 				sb.append(html.substring(lastPos, end));
 
 				return;
 			}
 
-			sb.append(html.substring(lastPos, imgStart));
+			sb.append(html.substring(start, imgStart));
 
 			int imgEnd = html.indexOf(CharPool.GREATER_THAN, imgStart) + 1;
 
@@ -248,7 +248,7 @@ public class AMBackwardsCompatibilityHtmlContentTransformer
 
 			int srcStart = html.indexOf(_ATTRIBUTE_TOKEN_SRC, attributeListPos);
 
-			if ((srcStart == -1) || (srcStart > imgEnd)) {
+			if (srcStart == -1) {
 				sb.append(html.substring(imgStart, imgEnd));
 
 				lastPos = imgEnd;
