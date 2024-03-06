966c1a9e4739b3aa0df53cf5c5f30e02ef40bb11
Roberto Díaz <roberto.diaz@liferay.com>
2024-03-06 20:42:07 +0100

    LPD-19203 Revert "LPS-205268 Additional tests"
    This reverts commit 0517a8710a0475c28f66d6b266770206f803edea.

--- modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/test/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformerTest.java
+++ modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/test/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformerTest.java
@@ -95,12 +95,6 @@ public class AMBackwardsCompatibilityHtmlContentTransformerTest {
 	}
 
 	@Test
-	public void testLeavesPictureTagsAsIs() throws Exception {
-		Assert.assertEquals(
-			_PICTURE_TAG, _contentTransformer.transform(_PICTURE_TAG));
-	}
-
-	@Test
 	public void testReplacesFriendlyURLImageTagsWithDoubleQuotes()
 		throws Exception {
 
@@ -115,10 +109,6 @@ public class AMBackwardsCompatibilityHtmlContentTransformerTest {
 			StringBundler.concat(
 				_CONTENT_PREFIX, "[REPLACED]", _PICTURE_TAG, _CONTENT_SUFFIX),
 			_contentTransformer.transform(_CONTENT_WITH_IMAGE_AND_PICTURE));
-		Assert.assertEquals(
-			StringBundler.concat(
-				_CONTENT_PREFIX, _PICTURE_TAG, "[REPLACED]", _CONTENT_SUFFIX),
-			_contentTransformer.transform(_CONTENT_WITH_PICTURE_AND_IMAGE));
 	}
 
 	@Test
@@ -225,12 +215,6 @@ public class AMBackwardsCompatibilityHtmlContentTransformerTest {
 		StringBundler.concat(
 			_CONTENT_PREFIX, "<img src=\"/documents/d/site_name/sample\" />",
 			_CONTENT_SUFFIX);
-
-	private static final String _CONTENT_WITH_PICTURE_AND_IMAGE =
-		StringBundler.concat(
-			_CONTENT_PREFIX,
-			AMBackwardsCompatibilityHtmlContentTransformerTest._PICTURE_TAG,
-			"<img src='/documents/d/site_name/sample' />", _CONTENT_SUFFIX);
 
 	private static final String _LEGACY_CONTENT_WITH_IMAGE_AND_SINGLE_QUOTES =
 		StringBundler.concat(
