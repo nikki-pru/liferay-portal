8de9152aeace813fa665600d00db9d0968db8d68
Carlos Montenegro <carlos.montenegro@liferay.com>
2024-04-08 09:22:02 -0300

    LPD-22734 exporting variable is never used for anything

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/shared/components/toolbar/UpperToolbar.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/shared/components/toolbar/UpperToolbar.js
@@ -107,13 +107,13 @@ export default function UpperToolbar({
 		}
 	};
 
-	const getXMLContent = (exporting) => {
+	const getXMLContent = () => {
 		let currentDescription;
 		let currentElements;
 		let currentName;
 		let xmlContent;
 
-		if (currentEditor && !exporting) {
+		if (currentEditor) {
 			xmlContent = currentEditor.getData();
 		}
 		else {
@@ -149,8 +149,7 @@ export default function UpperToolbar({
 					version,
 				},
 				currentElements.filter(isNode),
-				currentElements.filter(isEdge),
-				exporting
+				currentElements.filter(isEdge)
 			);
 		}
 
@@ -214,7 +213,7 @@ export default function UpperToolbar({
 
 		publishDefinitionRequest({
 			active,
-			content: getXMLContent(true),
+			content: getXMLContent(),
 			name: definitionName,
 			title: definitionTitle,
 			title_i18n: definitionTitleTranslations,
@@ -273,7 +272,7 @@ export default function UpperToolbar({
 
 		saveDefinitionRequest({
 			active,
-			content: getXMLContent(true),
+			content: getXMLContent(),
 			name: definitionName,
 			title: definitionTitle,
 			title_i18n: definitionTitleTranslations,
