03dd53b7dc41ceb6c5f9535bdf749bbf252128de
Carolina Barbosa <carolina.barbosa@liferay.com>
2024-04-08 09:47:58 -0300

    LPD-22327 Revert "LPD-18203 using structure version that fits the fields of the ddm content"
    This reverts commit bc3de249c29b4b40649ceae447865efe4be4dae1.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_1_3/DDMStorageLinkUpgradeProcess.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_1_3/DDMStorageLinkUpgradeProcess.java
@@ -7,20 +7,12 @@ package com.liferay.dynamic.data.mapping.internal.upgrade.v1_1_3;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
-import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
 import com.liferay.portal.kernel.upgrade.UpgradeStep;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * @author Pedro Queiroz
@@ -29,102 +21,35 @@ public class DDMStorageLinkUpgradeProcess extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		try (PreparedStatement selectDDMStorageLinkPreparedStatement =
-				connection.prepareStatement(
-					StringBundler.concat(
-						"select DDMStorageLink.storageLinkId, ",
-						"DDMStorageLink.structureId, DDMStorageLink.classPK, ",
-						"DDMContent.data_ from DDMStorageLink , DDMContent ",
-						"where DDMStorageLink.classPK = DDMContent.contentid"));
-			PreparedStatement selectDDMStructureVersionPreparedStatement =
-				connection.prepareStatement(
-					"select DDMStructureVersion.structureVersionId, " +
-						"DDMStructureVersion.definition from " +
-							"DDMStructureVersion where structureId = ?");
-			PreparedStatement updatePreparedStatement =
+		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
+				StringBundler.concat(
+					"select distinct DDMStorageLink.structureId, ",
+					"TEMP_TABLE.structureVersionId from DDMStorageLink inner ",
+					"join (select structureId, max(structureVersionId) as ",
+					"structureVersionId from DDMStructureVersion group by ",
+					"DDMStructureVersion.structureId) TEMP_TABLE on ",
+					"DDMStorageLink.structureId = TEMP_TABLE.structureId"));
+			PreparedStatement preparedStatement2 =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
 					"update DDMStorageLink set structureVersionId = ? where " +
-						"storageLinkId = ?");
-			ResultSet resultSet =
-				selectDDMStorageLinkPreparedStatement.executeQuery()) {
-
-			Map<Long, Long> ddmStorageLinkStructureVersionMap = new HashMap<>();
+						"structureId = ?");
+			ResultSet resultSet = preparedStatement1.executeQuery()) {
 
 			while (resultSet.next()) {
-				String data_ = resultSet.getString("data_");
+				long ddmStructureVersionId = resultSet.getLong(
+					"structureVersionId");
 
-				if ((data_ == null) || data_.isEmpty()) {
-					continue;
-				}
+				if (ddmStructureVersionId > 0) {
+					preparedStatement2.setLong(1, ddmStructureVersionId);
+					preparedStatement2.setLong(
+						2, resultSet.getLong("structureId"));
 
-				JSONObject jsonObject = JSONFactoryUtil.createJSONObject(data_);
-
-				JSONArray jsonArray = jsonObject.getJSONArray("fieldValues");
-
-				if (jsonArray == null) {
-					continue;
-				}
-
-				Set<String> ddmContentFieldNames = new HashSet<>(
-					jsonArray.length());
-
-				for (int i = 0; i < jsonArray.length(); i++) {
-					String fieldName = jsonArray.getJSONObject(
-						i
-					).getString(
-						"name"
-					);
-
-					ddmContentFieldNames.add(fieldName);
-				}
-
-				long structureId = resultSet.getLong("structureId");
-
-				selectDDMStructureVersionPreparedStatement.setLong(
-					1, structureId);
-
-				ResultSet resultSet2 =
-					selectDDMStructureVersionPreparedStatement.executeQuery();
-
-				long storageLinkId = resultSet.getLong("storageLinkId");
-
-				while (resultSet2.next()) {
-					Set<String> ddmFormFieldNames = new HashSet<>();
-
-					JSONObject definitionJSONObject =
-						JSONFactoryUtil.createJSONObject(
-							resultSet2.getString("definition"));
-
-					JSONArray fieldsJSONArray =
-						definitionJSONObject.getJSONArray("fields");
-
-					for (int i = 0; i < fieldsJSONArray.length(); i++) {
-						JSONObject fieldJSONObject =
-							fieldsJSONArray.getJSONObject(i);
-
-						ddmFormFieldNames.add(
-							fieldJSONObject.getString("name"));
-					}
-
-					if (ddmFormFieldNames.containsAll(ddmContentFieldNames)) {
-						ddmStorageLinkStructureVersionMap.put(
-							storageLinkId,
-							resultSet2.getLong("structureVersionId"));
-					}
+					preparedStatement2.addBatch();
 				}
 			}
 
-			for (Map.Entry<Long, Long> entry :
-					ddmStorageLinkStructureVersionMap.entrySet()) {
-
-				updatePreparedStatement.setLong(1, entry.getValue());
-				updatePreparedStatement.setLong(2, entry.getKey());
-
-				updatePreparedStatement.addBatch();
-			}
-
-			updatePreparedStatement.executeBatch();
+			preparedStatement2.executeBatch();
 		}
 	}
 
