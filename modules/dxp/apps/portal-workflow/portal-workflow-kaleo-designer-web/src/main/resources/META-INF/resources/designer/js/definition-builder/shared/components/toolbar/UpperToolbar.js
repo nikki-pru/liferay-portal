42ec622a34d9e3e91635a2a90852cf8fe28c674a
Carlos Montenegro <carlos.montenegro@liferay.com>
2024-04-08 19:08:24 -0300

    LPD-22734 Show invalid XML error as soons as blockingError is updated

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/shared/components/toolbar/UpperToolbar.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/shared/components/toolbar/UpperToolbar.js
@@ -382,12 +382,18 @@ export default function UpperToolbar({
 		if (blockingError.errorType === 'assignment') {
 			setAlert(blockingError.errorMessage, 'warning', true);
 		}
+		else if (blockingError.errorType === 'invalidXML') {
+			setAlert(blockingError.errorMessage, 'danger', true);
+		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [blockingError]);
 
 	const resetAlert = () => {
 		setShowAlert(false);
-		if (blockingError.errorType === 'assignment') {
+		if (
+			blockingError.errorType === 'assignment' ||
+			blockingError.errorType === 'invalidXML'
+		) {
 			setBlockingError({errorType: ''});
 		}
 	};
