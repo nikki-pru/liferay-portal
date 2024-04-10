b4c8e2b20996267a1db8b6741b2770a41c94b0b6
cdbm <christian.moura@liferay.com>
2024-04-10 10:24:52 -0300

    LPD-1617 adjust conditionals behavior

--- modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/LoginMVCActionCommand.java
+++ modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/LoginMVCActionCommand.java
@@ -128,6 +128,12 @@ public class LoginMVCActionCommand extends BaseMVCActionCommand {
 
 					SessionErrors.add(actionRequest, exception.getClass());
 				}
+
+				_postProcessAuthFailure(actionRequest, actionResponse);
+
+				hideDefaultErrorMessage(actionRequest);
+
+				return;
 			}
 			else if (exception instanceof
 						UserLockoutException.PasswordPolicyLockout) {
@@ -154,6 +160,12 @@ public class LoginMVCActionCommand extends BaseMVCActionCommand {
 
 				SessionErrors.add(
 					actionRequest, exception.getClass(), exception);
+
+				_postProcessAuthFailure(actionRequest, actionResponse);
+
+				hideDefaultErrorMessage(actionRequest);
+
+				return;
 			}
 
 			if (exception instanceof CompanyMaxUsersException ||
