b0bcd16950ce4ea11e9b6dff9263f2241e9b07d5
Brian Chan <brian.chan@liferay.com>
2024-04-25 11:24:41 -0300

    LPD-24038 SF

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/DocumentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/DocumentResourceTest.java
@@ -468,7 +468,6 @@ public class DocumentResourceTest extends BaseDocumentResourceTestCase {
 		DocumentType documentType = postDocument.getDocumentType();
 
 		Assert.assertNotNull(documentType);
-
 		Assert.assertEquals(
 			dlFileEntryType.getName(LocaleUtil.getDefault()),
 			documentType.getName());
