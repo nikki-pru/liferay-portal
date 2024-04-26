liferay/liferay-portal-cloud-native

LPD-22496 Regen

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryLocalService.java
@@ -445,8 +445,8 @@ public interface ObjectEntryLocalService
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<Map<String, Serializable>> getValuesList(
 			long groupId, long companyId, long userId, long objectDefinitionId,
-			List<String> fields, Predicate predicate, String search, int start,
-			int end, Sort[] sorts)
+			List<String> selectedObjectFieldNames, Predicate predicate,
+			String search, int start, int end, Sort[] sorts)
 		throws PortalException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryLocalServiceUtil.java
@@ -569,15 +569,15 @@ public class ObjectEntryLocalServiceUtil {
 
 	public static List<Map<String, Serializable>> getValuesList(
 			long groupId, long companyId, long userId, long objectDefinitionId,
-			List<String> fields,
+			List<String> selectedObjectFieldNames,
 			com.liferay.petra.sql.dsl.expression.Predicate predicate,
 			String search, int start, int end,
 			com.liferay.portal.kernel.search.Sort[] sorts)
 		throws PortalException {
 
 		return getService().getValuesList(
-			groupId, companyId, userId, objectDefinitionId, fields, predicate,
-			search, start, end, sorts);
+			groupId, companyId, userId, objectDefinitionId,
+			selectedObjectFieldNames, predicate, search, start, end, sorts);
 	}
 
 	public static int getValuesListCount(

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryLocalServiceWrapper.java
@@ -656,15 +656,16 @@ public class ObjectEntryLocalServiceWrapper
 	public java.util.List<java.util.Map<String, java.io.Serializable>>
 			getValuesList(
 				long groupId, long companyId, long userId,
-				long objectDefinitionId, java.util.List<String> fields,
+				long objectDefinitionId,
+				java.util.List<String> selectedObjectFieldNames,
 				com.liferay.petra.sql.dsl.expression.Predicate predicate,
 				String search, int start, int end,
 				com.liferay.portal.kernel.search.Sort[] sorts)
 		throws com.liferay.portal.kernel.exception.PortalException {
 
 		return _objectEntryLocalService.getValuesList(
-			groupId, companyId, userId, objectDefinitionId, fields, predicate,
-			search, start, end, sorts);
+			groupId, companyId, userId, objectDefinitionId,
+			selectedObjectFieldNames, predicate, search, start, end, sorts);
 	}
 
 	@Override

nikki-pru/liferay-portal

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryLocalService.java
@@ -445,8 +445,8 @@ public interface ObjectEntryLocalService
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<Map<String, Serializable>> getValuesList(
 			long groupId, long companyId, long userId, long objectDefinitionId,
-			List<String> fields, Predicate predicate, String search, int start,
-			int end, OrderByExpression[] orderByExpressions)
+			List<String> selectedObjectFieldNames, Predicate predicate,
+			String search, int start, int end, OrderByExpression[] orderByExpressions)
 		throws PortalException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryLocalServiceUtil.java
@@ -569,7 +569,7 @@ public class ObjectEntryLocalServiceUtil {
 
 	public static List<Map<String, Serializable>> getValuesList(
 			long groupId, long companyId, long userId, long objectDefinitionId,
-			List<String> fields,
+			List<String> selectedObjectFieldNames,
 			com.liferay.petra.sql.dsl.expression.Predicate predicate,
 			String search, int start, int end,
 			com.liferay.petra.sql.dsl.query.sort.OrderByExpression[]
@@ -577,8 +577,8 @@ public class ObjectEntryLocalServiceUtil {
 		throws PortalException {
 
 		return getService().getValuesList(
-			groupId, companyId, userId, objectDefinitionId, fields, predicate,
-			search, start, end, orderByExpressions);
+			groupId, companyId, userId, objectDefinitionId,
+			selectedObjectFieldNames, predicate, search, start, end, orderByExpressions);
 	}
 
 	public static int getValuesListCount(

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryLocalServiceWrapper.java
@@ -656,7 +656,8 @@ public class ObjectEntryLocalServiceWrapper
 	public java.util.List<java.util.Map<String, java.io.Serializable>>
 			getValuesList(
 				long groupId, long companyId, long userId,
-				long objectDefinitionId, java.util.List<String> fields,
+				long objectDefinitionId,
+				java.util.List<String> selectedObjectFieldNames,
 				com.liferay.petra.sql.dsl.expression.Predicate predicate,
 				String search, int start, int end,
 				com.liferay.petra.sql.dsl.query.sort.OrderByExpression[]
@@ -664,8 +665,8 @@ public class ObjectEntryLocalServiceWrapper
 		throws com.liferay.portal.kernel.exception.PortalException {
 
 		return _objectEntryLocalService.getValuesList(
-			groupId, companyId, userId, objectDefinitionId, fields, predicate,
-			search, start, end, orderByExpressions);
+			groupId, companyId, userId, objectDefinitionId,
+			selectedObjectFieldNames, predicate, search, start, end, orderByExpressions);
 	}
 
 	@Override
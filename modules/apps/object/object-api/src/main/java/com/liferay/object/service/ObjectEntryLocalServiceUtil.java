ae75a106cb6a7cd2fa5759feb0f70bcfdc49f00c
Brian Chan <brian.chan@liferay.com>
2024-04-22 15:35:00 -0300

    LPD-22496 Regen

--- modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryLocalServiceUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryLocalServiceUtil.java
@@ -569,7 +569,7 @@ public class ObjectEntryLocalServiceUtil {
 
 	public static List<Map<String, Serializable>> getValuesList(
 			long groupId, long companyId, long userId, long objectDefinitionId,
-			List<String> fields,
+			List<String> selectedObjectFieldNames,
 			com.liferay.petra.sql.dsl.expression.Predicate predicate,
 			String search, int start, int end,
 			com.liferay.petra.sql.dsl.query.sort.OrderByExpression[]
@@ -577,8 +577,8 @@ public class ObjectEntryLocalServiceUtil {
 		throws PortalException {
 
 		return getService().getValuesList(
-			groupId, companyId, userId, objectDefinitionId, fields, predicate,
-			search, start, end, orderByExpressions);
+			groupId, companyId, userId, objectDefinitionId,
+			selectedObjectFieldNames, predicate, search, start, end, orderByExpressions);
 	}
 
 	public static int getValuesListCount(
