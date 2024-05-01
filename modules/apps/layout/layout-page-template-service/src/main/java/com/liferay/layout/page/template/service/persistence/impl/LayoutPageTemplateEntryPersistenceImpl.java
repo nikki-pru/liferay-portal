0e784db06ce28d2277c665d445d8dc44618d583f
David Truong <david.truong@liferay.com>
2024-05-01 00:41:00 -0500

    LPD-24736 update LayoutPageTemplateEntry fields (lastPublishDate, statusByUserId, statusByUserName, statusDate) to merge during CT publish

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/persistence/impl/LayoutPageTemplateEntryPersistenceImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/persistence/impl/LayoutPageTemplateEntryPersistenceImpl.java
@@ -26840,6 +26840,7 @@ public class LayoutPageTemplateEntryPersistenceImpl
 	static {
 		Set<String> ctControlColumnNames = new HashSet<String>();
 		Set<String> ctIgnoreColumnNames = new HashSet<String>();
+		Set<String> ctMergeColumnNames = new HashSet<String>();
 		Set<String> ctStrictColumnNames = new HashSet<String>();
 
 		ctControlColumnNames.add("mvccVersion");
@@ -26861,16 +26862,17 @@ public class LayoutPageTemplateEntryPersistenceImpl
 		ctStrictColumnNames.add("defaultTemplate");
 		ctStrictColumnNames.add("layoutPrototypeId");
 		ctStrictColumnNames.add("plid");
-		ctStrictColumnNames.add("lastPublishDate");
+		ctMergeColumnNames.add("lastPublishDate");
 		ctStrictColumnNames.add("status");
-		ctStrictColumnNames.add("statusByUserId");
-		ctStrictColumnNames.add("statusByUserName");
-		ctStrictColumnNames.add("statusDate");
+		ctMergeColumnNames.add("statusByUserId");
+		ctMergeColumnNames.add("statusByUserName");
+		ctMergeColumnNames.add("statusDate");
 
 		_ctColumnNamesMap.put(
 			CTColumnResolutionType.CONTROL, ctControlColumnNames);
 		_ctColumnNamesMap.put(
 			CTColumnResolutionType.IGNORE, ctIgnoreColumnNames);
+		_ctColumnNamesMap.put(CTColumnResolutionType.MERGE, ctMergeColumnNames);
 		_ctColumnNamesMap.put(
 			CTColumnResolutionType.PK,
 			Collections.singleton("layoutPageTemplateEntryId"));
