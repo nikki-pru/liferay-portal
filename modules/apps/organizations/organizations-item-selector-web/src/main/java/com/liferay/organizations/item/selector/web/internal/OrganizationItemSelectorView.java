549142269cf2928102db80f4249c4f0e3cf0bb0c
João Victor Cordeiro <joao.cordeiro@liferay.com>
2024-05-02 15:30:22 -0300

    LPD-24824 only show organizations that the user manages/is a member in the Select Organizations Menu

--- modules/apps/organizations/organizations-item-selector-web/src/main/java/com/liferay/organizations/item/selector/web/internal/OrganizationItemSelectorView.java
+++ modules/apps/organizations/organizations-item-selector-web/src/main/java/com/liferay/organizations/item/selector/web/internal/OrganizationItemSelectorView.java
@@ -70,7 +70,8 @@ public class OrganizationItemSelectorView
 			organizationItemSelectorViewDisplayContext =
 				new OrganizationItemSelectorViewDisplayContext(
 					organizationItemSelectorCriterion,
-					_organizationLocalService, httpServletRequest, portletURL);
+					_organizationLocalService, httpServletRequest, _portal,
+					portletURL);
 
 		_itemSelectorViewDescriptorRenderer.renderHTML(
 			httpServletRequest, servletResponse,
