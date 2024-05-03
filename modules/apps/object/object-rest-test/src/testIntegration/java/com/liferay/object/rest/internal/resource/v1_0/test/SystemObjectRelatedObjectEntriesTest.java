34a90a405e24d483f2f414716a6d98ec943d64e3
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-05-03 16:00:17 +0200

    LPD-22412 Order matters and comments really add nothing here

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/SystemObjectRelatedObjectEntriesTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/SystemObjectRelatedObjectEntriesTest.java
@@ -265,14 +265,9 @@ public class SystemObjectRelatedObjectEntriesTest {
 
 		_testGetManyToOneSystemObjectRelatedObjectEntries(
 			StringPool.BLANK, 0, objectRelationship, _user.getUserId());
-
-		// New unrelated user
-
 		_testGetManyToOneSystemObjectRelatedObjectEntries(
 			StringPool.BLANK, 0, objectRelationship,
 			_userAccountJSONObject.getLong("id"));
-
-		// Default related user
 
 		ObjectRelationshipTestUtil.relateObjectEntries(
 			_objectEntry.getObjectEntryId(), _user.getUserId(),
@@ -282,8 +277,6 @@ public class SystemObjectRelatedObjectEntriesTest {
 			_objectEntry.getExternalReferenceCode(),
 			_objectEntry.getObjectEntryId(), objectRelationship,
 			_user.getUserId());
-
-		// New related user
 
 		ObjectRelationshipTestUtil.relateObjectEntries(
 			_objectEntry.getObjectEntryId(),
