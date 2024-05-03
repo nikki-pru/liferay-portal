a83acf4ceabeead7080068aa4322be4399a535c3
Pedro Tavares <pedro.tavares@liferay.com>
2024-05-03 18:03:44 -0300

    LPD-24689 Get the id of the actual creator user, instead of the current user

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/util/ObjectEntryVariablesUtil.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/util/ObjectEntryVariablesUtil.java
@@ -199,14 +199,13 @@ public class ObjectEntryVariablesUtil {
 
 		Map<String, Object> objectEntry =
 			(Map<String, Object>)payloadJSONObject.get("objectEntry");
-		String userId = payloadJSONObject.getString("userId");
 
 		Map<String, Object> allowedVariables =
 			HashMapBuilder.<String, Object>put(
 				"creator",
 				() -> {
 					if (objectDefinition.isUnmodifiableSystemObject()) {
-						return userId;
+						return null;
 					}
 
 					return MapUtil.getString(objectEntry, "userId");
@@ -229,7 +228,7 @@ public class ObjectEntryVariablesUtil {
 					return dateFormat.format(new Date());
 				}
 			).put(
-				"currentUserId", userId
+				"currentUserId", payloadJSONObject.getString("userId")
 			).put(
 				"groupId",
 				() -> {
@@ -258,6 +257,9 @@ public class ObjectEntryVariablesUtil {
 			if (variables == null) {
 				return payloadJSONObject.toMap();
 			}
+
+			allowedVariables.put(
+				"creator", MapUtil.getString(variables, "userId"));
 		}
 		else {
 			if (oldValues) {
