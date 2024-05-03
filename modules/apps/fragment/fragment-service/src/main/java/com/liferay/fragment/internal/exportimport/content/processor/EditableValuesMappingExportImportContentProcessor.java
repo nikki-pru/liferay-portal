8a1512097206e8a5987332934c554338a13fa17d
Victor Galan <victor.galan@liferay.com>
2024-05-03 10:08:05 +0200

    LPD-24852 Take into account collection mapping

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/exportimport/content/processor/EditableValuesMappingExportImportContentProcessor.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/exportimport/content/processor/EditableValuesMappingExportImportContentProcessor.java
@@ -129,7 +129,9 @@ public class EditableValuesMappingExportImportContentProcessor
 		throws Exception {
 
 		String mappedField = editableJSONObject.getString(
-			"mappedField", editableJSONObject.getString("fieldId"));
+			"collectionFieldId",
+			editableJSONObject.getString(
+				"mappedField", editableJSONObject.getString("fieldId")));
 
 		if (!mappedField.startsWith(_TEMPLATE)) {
 			return;
@@ -211,9 +213,12 @@ public class EditableValuesMappingExportImportContentProcessor
 
 		long classNameId = editableJSONObject.getLong("classNameId");
 		long classPK = editableJSONObject.getLong("classPK");
+		String collectionFieldId = editableJSONObject.getString(
+			"collectionFieldId");
 		String mappedField = editableJSONObject.getString("mappedField");
 
 		if (((classNameId == 0) || (classPK == 0)) &&
+			Validator.isNull(collectionFieldId) &&
 			Validator.isNull(mappedField)) {
 
 			return;
@@ -304,7 +309,9 @@ public class EditableValuesMappingExportImportContentProcessor
 		PortletDataContext portletDataContext, JSONObject editableJSONObject) {
 
 		String mappedField = editableJSONObject.getString(
-			"mappedField", editableJSONObject.getString("fieldId"));
+			"collectionFieldId",
+			editableJSONObject.getString(
+				"mappedField", editableJSONObject.getString("fieldId")));
 
 		if (mappedField.startsWith(_TEMPLATE)) {
 			long templateEntryId = GetterUtil.getLong(
@@ -317,7 +324,11 @@ public class EditableValuesMappingExportImportContentProcessor
 			long importedTemplateEntryId = MapUtil.getLong(
 				templateEntryIds, templateEntryId, templateEntryId);
 
-			if (editableJSONObject.has("mappedField")) {
+			if (editableJSONObject.has("collectionFieldId")) {
+				editableJSONObject.put(
+					"collectionFieldId", _TEMPLATE + importedTemplateEntryId);
+			}
+			else if (editableJSONObject.has("mappedField")) {
 				editableJSONObject.put(
 					"mappedField", _TEMPLATE + importedTemplateEntryId);
 			}
