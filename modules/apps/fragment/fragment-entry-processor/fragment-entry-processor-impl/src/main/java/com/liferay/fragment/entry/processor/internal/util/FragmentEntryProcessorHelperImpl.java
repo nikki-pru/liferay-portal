3754a062179553d138a753136460dcd608e11d71
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-05-21 14:07:21 +0200

    LPD-25739 Don't escape value if the text field is an URL

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-impl/src/main/java/com/liferay/fragment/entry/processor/internal/util/FragmentEntryProcessorHelperImpl.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-impl/src/main/java/com/liferay/fragment/entry/processor/internal/util/FragmentEntryProcessorHelperImpl.java
@@ -40,10 +40,14 @@ import com.liferay.portal.kernel.trash.TrashHandler;
 import com.liferay.portal.kernel.trash.TrashHandlerRegistryUtil;
 import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.KeyValuePair;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -423,6 +427,21 @@ public class FragmentEntryProcessorHelperImpl
 			}
 			else if (infoField.getInfoFieldType() instanceof
 						TextInfoFieldType) {
+
+				URI uri = null;
+
+				try {
+					uri = HttpComponentsUtil.getURI((String)value);
+				}
+				catch (URISyntaxException uriSyntaxException) {
+					if (_log.isDebugEnabled()) {
+						_log.debug(uriSyntaxException);
+					}
+				}
+
+				if (uri != null) {
+					return value;
+				}
 
 				return HtmlUtil.escape((String)value);
 			}
