ecbebd7fddbf6a5b777da3e0c69be11883e8f3b8
Brian Chan <brian.chan@liferay.com>
2024-05-27 15:48:57 -0300

    LPD-26187 Sublime sort

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/java/parser/ResourceOpenAPIParser.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/java/parser/ResourceOpenAPIParser.java
@@ -223,9 +223,9 @@ public class ResourceOpenAPIParser {
 
 			String parameterName = pathJavaMethodParameter.getParameterName();
 
-			if ((parameterName.endsWith("externalReferenceCode") ||
-				 parameterName.endsWith("ExternalReferenceCode") ||
-				 parameterName.endsWith("Path")) &&
+			if ((parameterName.endsWith("ExternalReferenceCode") ||
+				 parameterName.endsWith("Path") ||
+				 parameterName.endsWith("externalReferenceCode")) &&
 				Objects.equals(
 					pathJavaMethodParameter.getParameterType(),
 					"java.lang.String")) {
