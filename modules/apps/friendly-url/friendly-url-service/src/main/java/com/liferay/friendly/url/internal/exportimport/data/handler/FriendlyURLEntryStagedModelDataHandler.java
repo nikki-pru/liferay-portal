3e8fca4848a385c36f957efb6c03c2c87126c0ae
balazssk <balazs.safrany-kovalik@liferay.com>
2024-06-03 09:23:22 +0200

    LPD-27086 Remove redundancy

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/exportimport/data/handler/FriendlyURLEntryStagedModelDataHandler.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/exportimport/data/handler/FriendlyURLEntryStagedModelDataHandler.java
@@ -131,8 +131,6 @@ public class FriendlyURLEntryStagedModelDataHandler
 			importedFriendlyURLEntry =
 				(FriendlyURLEntry)friendlyURLEntry.clone();
 
-			importedFriendlyURLEntry.setDefaultLanguageId(
-				friendlyURLEntry.getDefaultLanguageId());
 			importedFriendlyURLEntry.setGroupId(
 				portletDataContext.getScopeGroupId());
 			importedFriendlyURLEntry.setCompanyId(
