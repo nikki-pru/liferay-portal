bda0161153d89814e733d4ee14ca0520cf7b0e5e
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-07-29 09:49:37 +0200

    LPD-28245 Add integration tests to assert the upgrade of the editable values of basic component fragment entry links

---
+++ modules/apps/layout/layout-page-template-test/src/testIntegration/java/com/liferay/layout/page/template/internal/upgrade/v3_4_1/test/FragmentEntryLinkEditableValuesUpgradeProcessTest.java
@@ -0,0 +1,305 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.page.template.internal.upgrade.v3_4_1.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.fragment.contributor.FragmentCollectionContributorRegistry;
+import com.liferay.fragment.entry.processor.constants.FragmentEntryProcessorConstants;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.layout.test.util.ContentLayoutTestUtil;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.cache.MultiVMPool;
+import com.liferay.portal.kernel.dao.orm.EntityCache;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@RunWith(Arquillian.class)
+public class FragmentEntryLinkEditableValuesUpgradeProcessTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_fragmentEntry =
+			_fragmentCollectionContributorRegistry.getFragmentEntry(
+				"BASIC_COMPONENT-heading");
+
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testUpgradeBorderRadius() throws Exception {
+		_assertUpgrade(
+			_jsonFactory.createJSONObject(), _jsonFactory.createJSONObject());
+		_assertUpgrade(
+			_jsonFactory.createJSONObject(),
+			JSONUtil.put("borderRadius", RandomTestUtil.randomString()));
+		_assertUpgrade(
+			JSONUtil.put("borderRadius", "0.375rem"),
+			JSONUtil.put("borderRadius", "lg"));
+		_assertUpgrade(
+			JSONUtil.put("borderRadius", StringPool.BLANK),
+			JSONUtil.put("borderRadius", "none"));
+		_assertUpgrade(
+			JSONUtil.put("borderRadius", "0.1875rem"),
+			JSONUtil.put("borderRadius", "sm"));
+	}
+
+	@Test
+	public void testUpgradeMarginBottom() throws Exception {
+		String expectedMarginBottom = String.valueOf(
+			RandomTestUtil.randomInt());
+
+		JSONObject expectedJSONObject = JSONUtil.put(
+			"marginBottom", expectedMarginBottom);
+
+		_assertUpgrade(
+			expectedJSONObject,
+			JSONUtil.put(
+				"bottomSpacing", expectedMarginBottom
+			).put(
+				"marginBottom", String.valueOf(RandomTestUtil.randomInt())
+			));
+		_assertUpgrade(
+			expectedJSONObject,
+			JSONUtil.put("marginBottom", expectedMarginBottom));
+	}
+
+	@Test
+	public void testUpgradeShadow() throws Exception {
+		_assertUpgrade(
+			_jsonFactory.createJSONObject(),
+			JSONUtil.put("boxShadow", RandomTestUtil.randomString()));
+		_assertUpgrade(
+			JSONUtil.put("shadow", "0 1rem 3rem rgba(0, 0, 0, .175)"),
+			JSONUtil.put("boxShadow", "lg"));
+		_assertUpgrade(
+			JSONUtil.put("shadow", "0 .125rem .25rem rgba(0, 0, 0, .075)"),
+			JSONUtil.put("boxShadow", "sm"));
+	}
+
+	@Test
+	public void testUpgradeTextAlign() throws Exception {
+		String expectedTextAlign = RandomTestUtil.randomString();
+
+		JSONObject expectedJSONObject = JSONUtil.put(
+			"textAlign", expectedTextAlign);
+
+		_assertUpgrade(
+			expectedJSONObject, JSONUtil.put("buttonAlign", expectedTextAlign));
+		_assertUpgrade(
+			expectedJSONObject,
+			JSONUtil.put("contentAlign", expectedTextAlign));
+		_assertUpgrade(
+			expectedJSONObject, JSONUtil.put("imageAlign", expectedTextAlign));
+		_assertUpgrade(
+			expectedJSONObject, JSONUtil.put("textAlign", expectedTextAlign));
+	}
+
+	@Test
+	public void testUpgradeTextColor() throws Exception {
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#DA1414"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "danger")));
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#272833"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "dark")));
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#393A4A"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "gray-dark")));
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#2E5AAC"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "info")));
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#F1F2F5"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "light")));
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#F7F8F9"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "lighter")));
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#0B5FFF"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "primary")));
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#6B6C7E"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "secondary")));
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#287D3C"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "success")));
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#B95000"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "warning")));
+		_assertUpgrade(
+			JSONUtil.put("textColor", "#FFFFFF"),
+			JSONUtil.put("textColor", JSONUtil.put("cssClass", "white")));
+
+		String expectedTextColor = RandomTestUtil.randomString();
+
+		_assertUpgrade(
+			JSONUtil.put("textColor", expectedTextColor),
+			JSONUtil.put(
+				"textColor", JSONUtil.put("cssClass", expectedTextColor)));
+
+		JSONObject expectedJSONObject = JSONUtil.put(
+			"textColor",
+			JSONUtil.put(
+				RandomTestUtil.randomString(), RandomTestUtil.randomString()));
+
+		_assertUpgrade(expectedJSONObject, expectedJSONObject);
+	}
+
+	private void _assertEditableValues(
+			JSONObject expectedJSONObject, FragmentEntryLink fragmentEntryLink)
+		throws Exception {
+
+		JSONObject editablesJSONObject = JSONFactoryUtil.createJSONObject(
+			fragmentEntryLink.getEditableValues());
+
+		JSONObject configurationJSONObject = editablesJSONObject.getJSONObject(
+			FragmentEntryProcessorConstants.
+				KEY_FREEMARKER_FRAGMENT_ENTRY_PROCESSOR);
+
+		Assert.assertTrue(
+			configurationJSONObject.toString(),
+			JSONUtil.equals(expectedJSONObject, configurationJSONObject));
+	}
+
+	private void _assertFragmentEntryLink(
+			JSONObject expectedJSONObject, long fragmentEntryLinkId,
+			Layout layout, long segmentsExperienceId)
+		throws Exception {
+
+		FragmentEntryLink fragmentEntryLink =
+			_fragmentEntryLinkLocalService.getFragmentEntryLink(
+				fragmentEntryLinkId);
+
+		Assert.assertEquals(layout.getPlid(), fragmentEntryLink.getPlid());
+		Assert.assertEquals(
+			segmentsExperienceId, fragmentEntryLink.getSegmentsExperienceId());
+
+		_assertEditableValues(expectedJSONObject, fragmentEntryLink);
+	}
+
+	private void _assertUpgrade(
+			JSONObject expectedJSONObject, JSONObject jsonObject)
+		throws Exception {
+
+		Layout layout = LayoutTestUtil.addTypeContentLayout(_group);
+
+		Layout draftLayout = layout.fetchDraftLayout();
+		long segmentsExperienceId =
+			_segmentsExperienceLocalService.fetchDefaultSegmentsExperienceId(
+				layout.getPlid());
+
+		FragmentEntryLink draftFragmentEntryLink =
+			ContentLayoutTestUtil.addFragmentEntryLinkToLayout(
+				JSONUtil.put(
+					FragmentEntryProcessorConstants.
+						KEY_FREEMARKER_FRAGMENT_ENTRY_PROCESSOR,
+					jsonObject
+				).toString(),
+				_fragmentEntry.getCss(), _fragmentEntry.getConfiguration(),
+				_fragmentEntry.getFragmentEntryId(), _fragmentEntry.getHtml(),
+				_fragmentEntry.getJs(), draftLayout,
+				_fragmentEntry.getFragmentEntryKey(), segmentsExperienceId,
+				_fragmentEntry.getType());
+
+		ContentLayoutTestUtil.publishLayout(draftLayout, layout);
+
+		FragmentEntryLink fragmentEntryLink =
+			_fragmentEntryLinkLocalService.getFragmentEntryLink(
+				layout.getGroupId(),
+				draftFragmentEntryLink.getFragmentEntryLinkId(),
+				layout.getPlid());
+
+		Assert.assertNotNull(fragmentEntryLink);
+
+		_runUpgrade();
+
+		_assertFragmentEntryLink(
+			expectedJSONObject, draftFragmentEntryLink.getFragmentEntryLinkId(),
+			draftLayout, segmentsExperienceId);
+		_assertFragmentEntryLink(
+			expectedJSONObject, fragmentEntryLink.getFragmentEntryLinkId(),
+			layout, segmentsExperienceId);
+	}
+
+	private void _runUpgrade() throws Exception {
+		UpgradeProcess upgradeProcess = UpgradeTestUtil.getUpgradeStep(
+			_upgradeStepRegistrator, _CLASS_NAME);
+
+		upgradeProcess.upgrade();
+
+		_entityCache.clearCache();
+		_multiVMPool.clear();
+	}
+
+	private static final String _CLASS_NAME =
+		"com.liferay.layout.page.template.internal.upgrade.v3_4_1." +
+			"FragmentEntryLinkEditableValuesUpgradeProcess";
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.layout.page.template.internal.upgrade.registry.LayoutPageTemplateServiceUpgradeStepRegistrator))"
+	)
+	private static UpgradeStepRegistrator _upgradeStepRegistrator;
+
+	@Inject
+	private EntityCache _entityCache;
+
+	@Inject
+	private FragmentCollectionContributorRegistry
+		_fragmentCollectionContributorRegistry;
+
+	private FragmentEntry _fragmentEntry;
+
+	@Inject
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private JSONFactory _jsonFactory;
+
+	@Inject
+	private MultiVMPool _multiVMPool;
+
+	@Inject
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
+
+}
