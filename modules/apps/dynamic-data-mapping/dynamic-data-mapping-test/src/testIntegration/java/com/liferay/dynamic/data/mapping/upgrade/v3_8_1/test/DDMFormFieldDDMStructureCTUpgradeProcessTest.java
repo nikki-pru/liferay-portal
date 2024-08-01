4be3b8a9b3ba1b97e9e5f01fe3540067debedb93
Carolina Barbosa <carolina.barbosa@liferay.com>
2024-07-31 21:19:22 -0300

    LPD-27759 Add integration tests

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/upgrade/v3_8_1/test/DDMFormFieldDDMStructureCTUpgradeProcessTest.java
@@ -0,0 +1,32 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.dynamic.data.mapping.upgrade.v3_8_1.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.upgrade.BaseDDMStructureCTUpgradeProcessTestCase;
+import com.liferay.dynamic.data.mapping.upgrade.v3_8_1.test.util.DDMFormFieldUpgradeProcessTestUtil;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author Carolina Barbosa
+ */
+@RunWith(Arquillian.class)
+public class DDMFormFieldDDMStructureCTUpgradeProcessTest
+	extends BaseDDMStructureCTUpgradeProcessTestCase {
+
+	@Override
+	protected String getClassName() {
+		return DDMFormFieldUpgradeProcessTestUtil.getClassName();
+	}
+
+	@Override
+	protected DDMForm getDDMForm() {
+		return DDMFormFieldUpgradeProcessTestUtil.getDDMForm();
+	}
+
+}
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/upgrade/v3_8_1/test/util/DDMFormFieldUpgradeProcessTestUtil.java
@@ -0,0 +1,42 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.dynamic.data.mapping.upgrade.v3_8_1.test.util;
+
+import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormTestUtil;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+/**
+ * @author Carolina Barbosa
+ */
+public class DDMFormFieldUpgradeProcessTestUtil {
+
+	public static String getClassName() {
+		return "com.liferay.dynamic.data.mapping.internal.upgrade.v3_8_1." +
+			"DDMFormFieldUpgradeProcess";
+	}
+
+	public static DDMForm getDDMForm() {
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm();
+
+		DDMFormField ddmFormField = DDMFormTestUtil.createDDMFormField(
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			DDMFormFieldTypeConstants.SELECT, "string", true, false, false);
+
+		ddmFormField.setPredefinedValue(
+			DDMFormValuesTestUtil.createLocalizedValue(
+				RandomTestUtil.randomString(), LocaleUtil.US));
+
+		ddmForm.addDDMFormField(ddmFormField);
+
+		return ddmForm;
+	}
+
+}
