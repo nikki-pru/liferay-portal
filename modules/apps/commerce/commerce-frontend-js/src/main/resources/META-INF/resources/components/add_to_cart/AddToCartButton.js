8eeaf63aadb8a9d4f6c0b711cf8532ada94a2f73
João Victor Cordeiro <joao.cordeiro@liferay.com>
2024-08-14 15:26:47 -0300

    LPD-3424 allow AddToCart button to be pressed more than once

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/add_to_cart/AddToCartButton.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/add_to_cart/AddToCartButton.js
@@ -103,11 +103,9 @@ function AddToCartButton({
 					onError(error);
 				})
 				.finally(() => {
-					if (isMounted()) {
-						setCartAtomState({updating: false});
+					setCartAtomState({updating: false});
 
-						setIsTriggeringCartUpdate(false);
-					}
+					setIsTriggeringCartUpdate(false);
 				});
 		},
 		// eslint-disable-next-line react-hooks/exhaustive-deps
