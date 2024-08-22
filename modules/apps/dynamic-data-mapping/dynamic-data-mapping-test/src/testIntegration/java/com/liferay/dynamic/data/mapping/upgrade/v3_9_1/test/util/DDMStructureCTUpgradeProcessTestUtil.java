3f2dd88045e69e41d21829eee08c574fb7046da7
Igor Costa <igor.costa@liferay.com>
2024-08-22 13:32:40 -0300

    LPD-27767 Apply CTUpgradeProcessTest pattern to tests from upgrade process v3_9_1

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/upgrade/v3_9_1/test/util/DDMStructureCTUpgradeProcessTestUtil.java
@@ -0,0 +1,43 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.dynamic.data.mapping.upgrade.v3_9_1.test.util;
+
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+
+import java.util.List;
+
+/**
+ * @author Igor Costa
+ */
+public class DDMStructureCTUpgradeProcessTestUtil {
+
+	public static DDMForm getDDMForm() {
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm(
+			RandomTestUtil.randomString());
+
+		List<DDMFormField> ddmFormFields = ddmForm.getDDMFormFields();
+
+		ddmFormFields.forEach(
+			ddmFormField -> {
+				if (ddmFormField.getFieldReference() != null) {
+					ddmFormField.setFieldReference(null);
+				}
+			});
+
+		ddmForm.setDDMFormFields(ddmFormFields);
+
+		return ddmForm;
+	}
+
+	public static String getUpgradeStepClassName() {
+		return "com.liferay.dynamic.data.mapping.internal.upgrade.v3_9_1." +
+			"DDMStructureUpgradeProcess";
+	}
+
+}
