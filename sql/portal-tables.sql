2eb32f53a680ffc4fd1f4e80431af9fa972d6b96
Manuele Castro <manuele.castro@liferay.com>
2024-09-10 18:05:11 -0300

    LPD-1909 buildService

--- sql/portal-tables.sql
+++ sql/portal-tables.sql
@@ -1142,6 +1142,16 @@ create table Release_ (
 	testString VARCHAR(1024) null
 );
 
+create table RememberMeToken (
+	mvccVersion LONG default 0 not null,
+	rememberMeTokenId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	createDate DATE null,
+	expirationDate DATE null,
+	token VARCHAR(75) null
+);
+
 create table Repository (
 	mvccVersion LONG default 0 not null,
 	ctCollectionId LONG default 0 not null,
