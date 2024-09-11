6ae4c497a57d4c1eb57e4b6cab93ba7a77bc70b3
Luis Ortiz <luis.ortiz@liferay.com>
2024-09-11 15:26:03 +0200

    LPD-35339 Adding integration test to check that NoSuchModelException and PrincipalException return the same message in APIs

---
+++ modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/internal/jaxrs/exception/mapper/test/ExceptionMapperTest.java
@@ -0,0 +1,133 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.vulcan.internal.jaxrs.exception.mapper.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.exception.NoSuchModelException;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.test.util.HTTPTestUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Collections;
+import java.util.Set;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Luis Ortiz
+ */
+@RunWith(Arquillian.class)
+public class ExceptionMapperTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() {
+		Bundle bundle = FrameworkUtil.getBundle(ExceptionMapperTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		_serviceRegistration = bundleContext.registerService(
+			Application.class, new ExceptionMapperTest.TestApplication(),
+			HashMapDictionaryBuilder.<String, Object>put(
+				"liferay.auth.verifier", true
+			).put(
+				"liferay.jackson", false
+			).put(
+				"liferay.oauth2", false
+			).put(
+				"osgi.jaxrs.application.base", _APPLICATION_BASE
+			).put(
+				"osgi.jaxrs.extension.select",
+				"(osgi.jaxrs.name=Liferay.Vulcan)"
+			).build());
+	}
+
+	@After
+	public void tearDown() {
+		_serviceRegistration.unregister();
+	}
+
+	@Test
+	public void testNoSuchModelExceptionAndPrincipalExceptionReturnTheSame()
+		throws Exception {
+
+		Assert.assertEquals(
+			HTTPTestUtil.invokeToHttpCode(
+				null,
+				_APPLICATION_BASE + _ENDPOINT_NO_SUCH_MODEL_EXCEPTION_PATH,
+				Http.Method.GET),
+			HTTPTestUtil.invokeToHttpCode(
+				null, _APPLICATION_BASE + _ENDPOINT_PRINCIPAL_EXCEPTION_PATH,
+				Http.Method.GET));
+
+		Assert.assertEquals(
+			HTTPTestUtil.invokeToJSONObject(
+				null,
+				_APPLICATION_BASE + _ENDPOINT_NO_SUCH_MODEL_EXCEPTION_PATH,
+				Http.Method.GET
+			).toString(),
+			HTTPTestUtil.invokeToJSONObject(
+				null, _APPLICATION_BASE + _ENDPOINT_PRINCIPAL_EXCEPTION_PATH,
+				Http.Method.GET
+			).toString());
+	}
+
+	public static class TestApplication extends Application {
+
+		@Override
+		public Set<Object> getSingletons() {
+			return Collections.singleton(this);
+		}
+
+		@GET
+		@Path(_ENDPOINT_NO_SUCH_MODEL_EXCEPTION_PATH)
+		@Produces("application/json")
+		public String testNoSuchModelException() throws NoSuchModelException {
+			throw new NoSuchModelException("Object cannot be found");
+		}
+
+		@GET
+		@Path(_ENDPOINT_PRINCIPAL_EXCEPTION_PATH)
+		@Produces("application/json")
+		public String testPrincipalException() throws PrincipalException {
+			throw new PrincipalException("User needs permissions to object");
+		}
+
+	}
+
+	private static final String _APPLICATION_BASE = "/test-vulcan";
+
+	private static final String _ENDPOINT_NO_SUCH_MODEL_EXCEPTION_PATH =
+		"/testNoSuchModelException";
+
+	private static final String _ENDPOINT_PRINCIPAL_EXCEPTION_PATH =
+		"/testPrincipalException";
+
+	private ServiceRegistration<Application> _serviceRegistration;
+
+}
