0ed729272b3eec079b8e71f1a6765a14af0d40b7
Joao Victor <joao.victor@liferay.com>
2024-10-16 15:44:04 -0300

    LPD-39385 Add test to validate changes

---
+++ modules/apps/blogs/blogs-test/src/testIntegration/java/com/liferay/blogs/internal/struts/test/FindEntryStrutsActionTest.java
@@ -0,0 +1,98 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.blogs.internal.struts.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.struts.StrutsAction;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * @author Joao Victor Alves
+ */
+@RunWith(Arquillian.class)
+public class FindEntryStrutsActionTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		ConfigurationTestUtil.deleteConfiguration(_PID);
+	}
+
+	@Test
+	public void testAllowedDomainsCanRedirect() throws Exception {
+		ConfigurationTestUtil.saveConfiguration(
+			_PID,
+			HashMapDictionaryBuilder.<String, Object>put(
+				"allowedDomains", new String[] {"*.google.com"}
+			).put(
+				"securityMode", "domain"
+			).build());
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.addParameter(
+			"noSuchEntryRedirect", "https://mail.google.com");
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setScopeGroupId(_group.getGroupId());
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		MockHttpServletResponse mockHttpServletResponse =
+			new MockHttpServletResponse();
+
+		_editInfoItemStrutsAction.execute(
+			mockHttpServletRequest, mockHttpServletResponse);
+
+		Assert.assertEquals(
+			"https://mail.google.com",
+			mockHttpServletResponse.getRedirectedUrl());
+	}
+
+	private static final String _PID =
+		"com.liferay.redirect.internal.configuration.RedirectURLConfiguration";
+
+	@DeleteAfterTestRun
+	private static Group _group;
+
+	@Inject(
+		filter = "component.name=com.liferay.blogs.web.internal.struts.FindEntryStrutsAction"
+	)
+	private StrutsAction _editInfoItemStrutsAction;
+
+}
