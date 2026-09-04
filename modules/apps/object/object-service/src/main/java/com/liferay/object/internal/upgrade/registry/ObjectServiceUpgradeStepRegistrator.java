2479a02afe4b1ba07d3d00e3968d74dc01009d57
Mario Gomes <mario.gomes@liferay.com>
2026-09-04 11:19:16 -0300

    LPD-100579 Adapt upgrade process to release-2026.q3

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -729,18 +729,6 @@ public class ObjectServiceUpgradeStepRegistrator
 
 		registry.register(
 			"13.1.0", "13.2.0",
-			new com.liferay.object.internal.upgrade.v13_2_0.
-				ObjectDefinitionExternalReferenceCodeUpgradeProcess(
-					_systemObjectDefinitionManagerRegistry));
-
-		registry.register(
-			"13.2.0", "13.3.0",
-			new AttachmentObjectFieldDownloadPermissionUpgradeProcess(
-				_language, _localization, _ploEntryLocalService,
-				_resourceActionLocalService));
-
-		registry.register(
-			"13.3.0", "13.4.0",
 			new BaseExternalReferenceCodeUpgradeProcess() {
 
 				@Override
