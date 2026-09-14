25abe989e956c5429cee07deb2f01819e7d03150
Vendel Töreki <vendel.toreki@liferay.com>
2026-09-14 11:35:26 +0200

    LPD-102315 Share the error message lookup

--- modules/apps/export-import/export-import-rest-impl/src/main/java/com/liferay/exportimport/rest/internal/resource/v1_0/ExportProcessResourceImpl.java
+++ modules/apps/export-import/export-import-rest-impl/src/main/java/com/liferay/exportimport/rest/internal/resource/v1_0/ExportProcessResourceImpl.java
@@ -32,8 +32,6 @@ import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.dao.orm.OrderFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.NoSuchBackgroundTaskException;
-import com.liferay.portal.kernel.json.JSONFactory;
-import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
@@ -358,12 +356,6 @@ public class ExportProcessResourceImpl extends BaseExportProcessResourceImpl {
 		return group.getGroupId();
 	}
 
-	private String _getDefaultErrorMessage() {
-		return _language.get(
-			contextAcceptLanguage.getPreferredLocale(),
-			"an-unexpected-error-occurred");
-	}
-
 	private DynamicQuery _getDynamicQuery(
 		Long creatorId, long groupId, String portletId, String search,
 		Integer status) {
@@ -619,24 +611,9 @@ public class ExportProcessResourceImpl extends BaseExportProcessResourceImpl {
 				setDateCreated(backgroundTask::getCreateDate);
 				setDateModified(backgroundTask::getModifiedDate);
 				setErrorMessage(
-					() -> {
-						if (backgroundTask.getStatus() !=
-								BackgroundTaskConstants.STATUS_FAILED) {
-
-							return null;
-						}
-
-						JSONObject jsonObject =
-							_jsonFactory.safeCreateJSONObject(
-								backgroundTask.getStatusMessage(), true);
-
-						if (jsonObject == null) {
-							return _getDefaultErrorMessage();
-						}
-
-						return jsonObject.getString(
-							"message", _getDefaultErrorMessage());
-					});
+					() -> BackgroundTaskUtil.getErrorMessage(
+						backgroundTask,
+						contextAcceptLanguage.getPreferredLocale()));
 				setId(backgroundTask::getBackgroundTaskId);
 				setName(() -> BackgroundTaskUtil.getName(backgroundTask));
 				setStatus(
@@ -689,9 +666,6 @@ public class ExportProcessResourceImpl extends BaseExportProcessResourceImpl {
 
 	@Reference
 	private ExportImportLocalService _exportImportLocalService;
-
-	@Reference
-	private JSONFactory _jsonFactory;
 
 	@Reference
 	private Language _language;
